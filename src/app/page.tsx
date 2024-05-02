e8d17e4744b193c903b79fc43819f2af6d9de76a
mshaikh4 <mshai1>
2024-05-01 21:04:36 -0400

    updated the content and added a link to my resume and projects as well

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 
+
 // ProfileInfo component
 const ProfileInfo = () => (
   <div className="flex flex-col items-center justify-between p-8 mb-8">
@@ -15,70 +16,38 @@ const ProfileInfo = () => (
       </div>
       <h1 className="text-3xl font-semibold mb-2">Mohammad S. Shaikh</h1>
       <p className="text-lg mb-2">Hey there! I'm Mohammad Shaikh, a fresh computer science graduate ready to dive headfirst into the exciting world of technology. Throughout my studies, I've become pretty handy with coding and understanding how computers tick. I'm always curious and love finding creative solutions to challenges. My friends and professors know me as someone who's dedicated and loves working with others. Now, I'm pumped to take what I've learned and make a positive impact in the tech universe. Let's do this!</p>
-      <p className="text-lg mb-2">If you're looking to connect, collaborate, or just chat about the latest innovations, feel free to drop me a line at <a href="mailto:mohammad2shaikh27@gmail.com">mohammad2shaikh27@gmail.com</a>. Let's spark some ideas and make waves in the world of technology together! </p>
+      <p className="text-lg mb-2">If you're looking to connect, collaborate, or just chat about the latest innovations, feel free to drop me a line at <a href="mailto:mohammad2shaikh27@gmail.com" style={{ textDecoration: 'underline' }}>mohammad2shaikh27@gmail.com</a>. Let's spark some ideas and make waves in the world of technology together! </p>
     </div>
-    <div className="grid gap-2">
-      <h2 className="text-xl font-semibold">EDUCATION</h2>
-      <p>Siena College, Loudonville, NY</p>
-      <p>Bachelor of Computer in Computer Science</p>
-      <p>GPA: 3.69</p>
-      <p>Hudson Valley Community College, Troy, NY</p>
-      <p>Associate of Computer in Computer Science</p>
-      <p>GPA: 3.77</p>
-      <h2 className="text-xl font-semibold">RELEVANT SKILLS</h2>
-      <p>Familiar with: Swift, Java, HTML/CSS, JavaScript, Relational Database, Assembly, C#, Command Line</p>
-      <p>Proficient in: Xcode, Figma, VS code, GitHub, Oracle, O365 & Teams, SCCM, Intune, ServiceNow, Flownia</p>
-      <h2 className="text-xl font-semibold">RELEVANT EXPERIENCE</h2>
-      <p>Product Owner, Siena College, Latham, NY (Sept. 2023 - Now)</p>
-      <ul>
-        <li>Ensuring seamless communication among stakeholders, clients, and the development team to align on project goals, while leveraging creativity to introduce innovative solutions and enhance software development.</li>
-        <li>Exhibiting effective collaboration within diverse teams, emphasizing understanding and cooperation among members with differing viewpoints.</li>
-      </ul>
-      {/* Additional experience and honors can be added similarly */}
-    </div>
+      <div className="grid gap-2">
+      <h2 className="text-xl font-semibold">PROJECTS</h2>
+      <p>As someone deeply passionate about problem-solving through technology, I've embarked on various projects that highlight my skills and creativity. These projects serve as testaments to my ability to conceptualize, develop, and deliver software solutions that address real-world challenges. Check out some of my projects:</p>
+    <ul>
+      <li>
+        <a href="https://github.com/mshai1/coffeehub" style={{ textDecoration: 'underline' }}>CoffeeHub</a> - CoffeeHub, a React-based web app, replicates the coffee-making process. Using Bootstrap, it offers Home, OfferedCoffee, and MakeCoffee pages for browsing, brewing, and earning. Stock replenishes after use, making it ideal for mastering React and Bootstrap.
+      </li>
+      <br />
+      <li>
+        <a href="https://github.com/mshai1/Todo" style={{ textDecoration: 'underline' }}>Todo</a> - An uncomplicated Java-based Todo List application designed to enable users in task management via either a terminal or graphical user interface (GUI).
+      </li>
+      {/* Add more projects similarly */}
+    </ul>
+      </div>
   </div>
 );
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
       <ProfileInfo />
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started bçy editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
+
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
@@ -133,19 +102,19 @@ export default function Home() {
         </a>
 
         <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="/Resume.pdf"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
+            Resume{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Click on the link below to download a copy of my resume
           </p>
         </a>
       </div>
